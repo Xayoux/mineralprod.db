04d02a7a2e57af09982376bae629f2e14acb377e
Romain Capliez <romain.capliez01@gmail.com>
2024-03-07 12:40:57 +0100

    Aujout de la fonction launch_download_myb

---
+++ R/launch_download_myb.R
@@ -0,0 +1,30 @@
+#' Permet de lancer le processus de téléchargement du myb en limitant les risques d'erreur grâce à un fichier compilant les messages d'erreur.
+#'
+#' @param path_usgs_myb Un lien vers la page d'acceuil du myb de USGS. Le lien est rentré par défaut.
+#'
+#' @return Tous les fichiers Excel du myb.
+#' @export
+#'
+#' @examples # Pas d'exemple.
+launch_download_myb <- function(
+    path_usgs_myb = "https://www.usgs.gov/centers/national-minerals-information-center/minerals-yearbook-metals-and-minerals"
+){
+
+  # Supprimer le fichier d'erreur globale s'il existe
+  if(file.exists("messages-errors-global.txt")){
+    file.remove("messages-errors-global.txt")
+  }
+
+  # Tester le code
+  tryCatch({
+
+    # Exécute la fonction pour télécharger tout le myb
+    mineralprod.db::download_all_myb(path_usgs_myb)
+
+  }, error = function(e){
+    # Ecris un fichier contenant les messages d'erreurs qui pourraient survenir
+    messages_error_global <- file("messages-errors-global.txt", open = "a")
+    writeLines(e$message, messages_error_global)
+    close(messages_error_global)
+  })
+}
