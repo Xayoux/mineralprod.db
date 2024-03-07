7ceff4202b4772ff893aa0bf77f567a1e654b601
Romain Capliez <romain.capliez01@gmail.com>
2024-03-07 12:23:23 +0100

    Passage en français + création de la fonction download_one_mineral

--- R/download_one_file.R
+++ R/download_one_file.R
@@ -1,25 +1,34 @@
-#' Allow to download a file with is URL if it doesn't exist already. Write an error message in a document if there is an error.
+#' Permet de télécharger un ficheir à partir de son url et d'enregistrer un message d'erreur s'il y en a.
 #'
-#' @param excel_file_link An url link leading to an excel file
-#' @param excel_file_path A path indicating where the file should be save
+#' @param excel_file_link Une URl vers un fichier.
+#' @param excel_file_path Un chemin d'accès vers l'endroit où le ficheir doit être enregistré.
 #'
-#' @return A saving file or an error document
+#' @return Un fichier téléchargé et/ou un document d'erreur.
 #' @export
 #'
-#' @examples #No example
+#' @examples # Pas d'exemple
 download_one_file <- function(excel_file_link, excel_file_path){
+
+  # Teste si le fichier existe déjà
   if (file.exists(excel_file_path) == FALSE){
+
+    # Essaie le code
     tryCatch({
+
+      # Télécharge le fichier et l'enregistre au bon endroit
       utils::download.file(
         excel_file_link,
         destfile = excel_file_path,
         mode = "wb"
       )
     }, error = function(e){
+
+      # S'il y a une erreur enregistre un fichier avec le message d'erreur
       messages_error_download_file <- file("messages-errors-downloads.txt", open = "a")
       writeLines(paste(excel_file_link, "\n", e$message, "\n\n"), messages_error_download_file)
       close(messages_error_download_file)
 
+      # S'il y a une erreur enregistrer dans un fichier le lien
       links_download_file <- file("links-errors-downloads.txt", open = "a")
       writeLines(excel_file_link, links_download_file)
       close(links_download_file)
---
+++ R/download_one_mineral.R
@@ -0,0 +1,49 @@
+#' Permet de télécharger tous les fichiers excel d'une page du myb USGS
+#'
+#' @param mineral_page_link Un lien URl vers une page du myb pour un minerais
+#'
+#' @return Chaque fichier du pyb téléchargé pour le minerais en question
+#' @export
+#'
+#' @examples # Pas d'exemple
+download_one_mineral <- function(mineral_page_link){
+
+  # Essaie le code et catch s'il y a une erreur
+  tryCatch({
+
+    # Lis la page HTML du minerais souhaité
+    html <- rvest::read_html(mineral_page_link)
+
+    # Stocke tous les liens de fichiers xls et xlsx de la page du minerais en cours
+    excel_link_list <-
+      html |>
+      rvest::html_nodes(".tex2jax_process") |>
+      rvest::html_elements("ul") |>
+      rvest::html_elements("li") |>
+      rvest::html_elements("a") |>
+      rvest::html_attr("href") %>%
+      { .[grep("\\.(xlsx|xls)$", .)]} %>%
+      { .[!grepl("\\d{4}q\\d", .)] } # Enlève les quaterly
+
+    # Créer le répertoire pour les fichiers du minerais
+    repertory_path <-
+      here::here(
+        "01-data",
+        sub(".*/", "", mineral_page_link) # Garder que le nom du minerais
+      )
+
+    ifelse(dir.exists(repertory_path) == FALSE, dir.create(repertory_path))
+
+    # Créer les liens d'enregistrement des fichiers
+    liste_path <-
+      paste(
+        repertory_path,
+        "/",
+        sub(".*/", "", excel_link_list), # Garder que le nom du fichier
+        sep = ""
+      )
+
+    # Télécharger tous les fichiers de la page en cours
+    purrr::walk2(excel_link_list, liste_path, mineralprod.db::download_one_file)
+  }, error = function(e){})
+}
